a2125c00c60ae46f038e549d4593790cf3e65c08
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-03-04 22:10:20 +0000

    Use external function in 'effect'

--- src/singleColour.glsl
+++ src/singleColour.glsl
@@ -3,6 +3,13 @@
 // texture_coords represent the normalised coordinates of the of the current pixel relative to the image
 // screen_coords represent the normalised coordinates of the of the current pixel relative to the screen
 
+vec4 red();
+
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords){
-    return vec4(1.0, 0.0, 1.0, 1.0);
+    // return vec4(1.0, 0.0, 1.0, 1.0);
+    return red();
+}
+
+vec4 red() {
+    return vec4(0.75, 0.0, 0.0, 1.0);
 }
