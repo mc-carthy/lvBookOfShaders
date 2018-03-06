7ca7b42e7f0a7ef50a269d124851b07a494ab326
mc-carthy <mick@mccarthy.tech>
2018-03-06 15:30:01 +0000

    Combining smoothSteps

--- src/shaders/smoothStep.glsl
+++ src/shaders/smoothStep.glsl
@@ -8,7 +8,8 @@ float plot(vec2 st, float pct) {
 
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords){
     vec2 st = vec2(screen_coords.x / resolution.x, 1 - (screen_coords.y / resolution.y));
-    float y = smoothstep(0.1, 0.9, st.x);
+    // float y = smoothstep(0.1, 0.9, st.x);
+    float y = smoothstep(0.2,0.5,st.x) - smoothstep(0.5,0.8,st.x);
     vec3 colour = vec3(y);
 
     // Plot a line
